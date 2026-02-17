b509da4bab7c29473877f64b63730a5d6432f62c
dialika740 <dialikandong740@gmail.com>
2026-02-17 09:05:32 +0000

    push Jour 2

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -3,17 +3,24 @@ import { createContext, useState, useContext } from 'react'
 const AuthContext = createContext(null)
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(() => {
+    // Vérifie si un utilisateur est déjà stocké (persistance)
+    const saved = localStorage.getItem("user")
+    return saved ? JSON.parse(saved) : null
+
+  })
 
   const login = (email, password) => {
     // Simulation de connexion
-    setUser({ email, name: 'Étudiant' })
+    const  fakeUser={ email, name: 'Étudiant' }
+    setUser(fakeUser)
+    localStorage.setItem("user", JSON.stringify(fakeUser))
     return true   // retourne true pour signaler succès
   }
 
-
   const logout = () => {
     setUser(null)
+    localStorage.removeItem("user")
   }
 
   return (
@@ -22,7 +29,6 @@ export const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   )
 }
-
 export const useAuth = () => {
   const context = useContext(AuthContext)
   if (!context) throw new Error('useAuth must be used within AuthProvider')
